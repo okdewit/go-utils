fd726921efad93a64489194b6f2710c91996e5d6
Orian de Wit <okdewit@gmail.com>
2019-10-08 23:00:58 +0200

    fix

--- timeutils/timeutils.go
+++ timeutils/timeutils.go
@@ -3,6 +3,6 @@ package timeutils
 import "time"
 
 func QuarterBoundaries(y int, quarter int) (a time.Time, b time.Time) {
-	return time.Date(y, time.Month(quarter*(quarter-1)+1), 1, 0, 0, 0, 0, time.UTC),
-		time.Date(y,time.Month(quarter*(quarter-1)+4), 1, 0, 0, 0, -1, time.UTC)
+	return time.Date(y, time.Month((quarter-1)*3+1), 1, 0, 0, 0, 0, time.UTC),
+		time.Date(y,time.Month((quarter-1)*3+4), 1, 0, 0, 0, -1, time.UTC)
 }
--- timeutils/timeutils_test.go
+++ timeutils/timeutils_test.go
@@ -6,11 +6,27 @@ import (
 )
 
 func TestQuarterBoundaries(t *testing.T) {
-	s, e := QuarterBoundaries(2019, 3)
-	assert.Equal(t, "2019-07-01", s.Format("2006-01-02"))
-	assert.Equal(t, "2019-09-30", e.Format("2006-01-02"))
+	t.Run("Q1", func(t *testing.T) {
+		s, e := QuarterBoundaries(2010, 1)
+		assert.Equal(t, "2010-01-01", s.Format("2006-01-02"))
+		assert.Equal(t, "2010-03-31", e.Format("2006-01-02"))
+	})
 
-	s, e = QuarterBoundaries(2010, 1)
-	assert.Equal(t, "2010-01-01", s.Format("2006-01-02"))
-	assert.Equal(t, "2010-03-31", e.Format("2006-01-02"))
+	t.Run("Q2", func(t *testing.T) {
+		s, e := QuarterBoundaries(1900, 2)
+		assert.Equal(t, "1900-04-01", s.Format("2006-01-02"))
+		assert.Equal(t, "1900-06-30", e.Format("2006-01-02"))
+	})
+
+	t.Run("Q3", func(t *testing.T) {
+		s, e := QuarterBoundaries(2021, 3)
+		assert.Equal(t, "2021-07-01", s.Format("2006-01-02"))
+		assert.Equal(t, "2021-09-30", e.Format("2006-01-02"))
+	})
+
+	t.Run("Q4", func(t *testing.T) {
+		s, e := QuarterBoundaries(1501, 4)
+		assert.Equal(t, "1501-10-01", s.Format("2006-01-02"))
+		assert.Equal(t, "1501-12-31", e.Format("2006-01-02"))
+	})
 }
