9fcee860741e633a29446df8c9f2621fbee7c4d8
Orian de Wit <okdewit@gmail.com>
2019-10-09 20:03:10 +0200

    timeutils

--- timeutils/timeutils.go
+++ timeutils/timeutils.go
@@ -5,11 +5,6 @@ import (
 	"time"
 )
 
-func QuarterBoundaries(y int, quarter int) (a time.Time, b time.Time) {
-	return time.Date(y, time.Month((quarter-1)*3+1), 1, 0, 0, 0, 0, time.UTC),
-		time.Date(y,time.Month((quarter-1)*3+4), 1, 0, 0, 0, -1, time.UTC)
-}
-
 func FmtDuration(d time.Duration) string {
 	d = d.Round(time.Minute)
 	x := d / (time.Hour * 24)
@@ -22,3 +17,42 @@ func FmtDuration(d time.Duration) string {
 	}
 	return fmt.Sprintf("%02d:%02d", h, m)
 }
+
+type Quarter struct {
+	Y int;
+	Q int;
+}
+
+
+func toQuarter(t time.Time) (Quarter) {
+	return Quarter{
+		Y: t.Year(),
+		Q: int((time.Now().Month()+2)/3),
+	}
+}
+
+func (quarter *Quarter) String() string {
+	return string(quarter.Y) + "-Q" + string(quarter.Q)
+}
+
+func (quarter Quarter) IsCurrent() bool {
+	current := toQuarter(time.Now())
+	return quarter.Y == current.Y && quarter.Q == current.Q
+}
+
+func (quarter Quarter) IsFuture() bool {
+	current := toQuarter(time.Now())
+	return quarter.Y > current.Y || (quarter.Y == current.Y && quarter.Q > current.Q)
+}
+
+func (quarter Quarter) IsPast() bool {
+	current := toQuarter(time.Now())
+	return quarter.Y < current.Y || (quarter.Y == current.Y && quarter.Q < current.Q)
+}
+
+func (quarter *Quarter) Boundaries() (a time.Time, b time.Time) {
+	return time.Date(quarter.Y, time.Month((quarter.Q-1)*3+1), 1, 0, 0, 0, 0, time.UTC),
+		time.Date(quarter.Y,time.Month((quarter.Q-1)*3+4), 1, 0, 0, 0, -1, time.UTC)
+}
+
+
